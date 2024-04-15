02efb0983dcdd7ee08c6a8e4d7b7870b1e35022b
porhuat <gohporhuat@hotmail.com>
2024-04-15 17:51:18 +0800

    Add Character & DataManager

---
+++ Assets/_Scripts/DataManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    //玩家儲存點位置
+    [SerializeField] Transform _savePointPosition = null;
+
+    //玩家初始位置
+    [SerializeField] Transform _playerInitialPosition = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            PlayerPrefs.SetString("playerSavePoint", _savePointPosition.position.ToString());
+            Debug.Log("savePoint:" + _savePointPosition.position.ToString());
+        }
+    }
+}
