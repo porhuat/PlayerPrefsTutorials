afeb349aa25ab7dab90bccbd78d472a15f8d73c9
porhuat <gohporhuat@hotmail.com>
2024-04-17 17:24:51 +0800

    Supplementary full functionality

--- Assets/_Scripts/DataManager.cs
+++ Assets/_Scripts/DataManager.cs
@@ -1,33 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class DataManager : MonoBehaviour
+//Enum to represent different button actions(表示不同按鈕的操作)
+public enum UIButtonAction
 {
-    //玩家儲存點位置
-    [SerializeField] Transform _savePointPosition = null;
+    DELETE = 0
+}
 
-    //玩家初始位置
-    [SerializeField] Transform _playerInitialPosition = null;
+public class DataManager : Singleton<DataManager>
+{
+    //Save Points (儲存點)
+    public Transform savePointPosition;
 
-    // Start is called before the first frame update
+    //Player Initial Position (玩家初始位置)
+    [SerializeField] Transform _playerInitialPosition;
+
+    //Player Current Position (玩家當前位置)
+    [SerializeField] Transform _playerCurrentPosition;
+
+    //Delete Save Record Button (刪除記錄按鍵)
+    [SerializeField] Button _btnDelectSaveRecord = null;
+
     void Start()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.CompareTag("Player"))
+        //檢查PlayerPrefs 是否有“playerSavePoint” 值
+        if (PlayerPrefs.HasKey("playerSavePoint"))
         {
-            PlayerPrefs.SetString("playerSavePoint", _savePointPosition.position.ToString());
-            Debug.Log("savePoint:" + _savePointPosition.position.ToString());
+            Debug.Log("PlayerPrefs: have player position key");
+
+            //將savePointPosition的值，賦予給_playerCurrentPosition
+            _playerCurrentPosition.transform.position = savePointPosition.position;
+        }
+        else
+        {
+            Debug.Log("PlayerPrefs: No saved player position found");
+
+            //找不到PlayerPrefs内有任何儲存玩家的坐標值，所以將玩家初始坐標，賦值給 _playerCurrentPosition
+            _playerCurrentPosition.transform.position = _playerInitialPosition.position;
+        }
+
+        //Add a listener for the Delete button click (在刪除記錄按鍵添加監聽器)
+        _btnDelectSaveRecord.onClick.AddListener(() => ButtonAction((int)UIButtonAction.DELETE));
+    }
+
+    private void ButtonAction(int index)
+    {
+        //Handle button action based on the provided enum value(根據提供的Enum value 去處理按鈕操作)
+        switch (index)
+        {
+            case (int)UIButtonAction.DELETE:
+                Debug.LogWarning("Delete Save Record");
+
+                //Removes all keys and values from the preferences
+                //删除PlayerPrefs内所有值
+                PlayerPrefs.DeleteAll();
+
+                //Removes the given key from the PlayerPrefs. If the key does not exist, DeleteKey has no impact.
+                //从 PlayerPrefs 中删除给定的值。如果值不存在，DeleteKey 不會產生任何影响。
+                PlayerPrefs.DeleteKey("playerSavePoint");
+                break;
+
+            default:
+                break;
         }
     }
 }
---
+++ Assets/_Scripts/SavePointDetect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavePointDetect : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        //Detects if it is a player and saves a record of the coordinates of the Save Points in PlayerPrefs
+        //偵測如果是玩家，並將存儲點的坐標記錄保存在PlayerPrefs
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PlayerPrefs.SetString("playerSavePoint", DataManager.Instance.savePointPosition.transform.position.ToString());
+            Debug.Log("Player position saved");
+        }
+    }
+}
